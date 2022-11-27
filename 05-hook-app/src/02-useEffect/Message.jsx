6f2a377fe79519e44a80535564901b9485ede171
kevinorh <kevinorh@hotmail.com>
2022-11-27 16:07:31 -0500

    Avance

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
-    useEffect(() => {
-      console.log('Message Mounted')
-    
-      return () => {
-        console.log('Message Unmounted')
-      }
-    }, [])
-    
+  const [coords, setCoords] = useState({x:0, y:0})
 
-    return (
-        <>
-            <h3>Usuario ya existe</h3>
-        </>
-    )
+  useEffect(() => {
+    const onMouseMove = ({x,y}) =>{
+      // const coords = {x,y};
+      setCoords({x,y})
+    }
+    // console.log('Message Mounted')
+    // window.addEventListener('mousemove',(event) => {
+    //   console.log(event.x, event.y);
+    // })
+    window.addEventListener('mousemove',onMouseMove)
+    return () => {
+      // console.log('Message Unmounted')
+      window.removeEventListener('mousemove',onMouseMove)
+    }
+  }, [])
+
+
+  return (
+    <>
+      <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)}
+    </>
+  )
 }
