7f834d76fc88809abaa24acf0249e5e5d7feb97c
kevinorh <kevinorh@hotmail.com>
2022-11-29 00:07:44 -0500

    useMemo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -7,12 +7,13 @@ import ReactDOM from 'react-dom/client'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLaoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+// import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>
 )
